492a18ad81a9c370d98f6e9dd41b692618307ea6
ww386362087 <tongwuwei@163.com>
2019-09-12 22:40:08 +0800

    modif RadarChart file

--- Assets/Game/GameFramework/UI/UITools/RadarChartGraphic.cs
+++ Assets/Game/GameFramework/UI/UITools/RadarChartGraphic.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 
 namespace GameFramework.UI.UITools
 {
+    //点击检测只实现外框的范围 中间镂空的没有处理
     public class RadarChartGraphic : MaskableGraphic , ICanvasRaycastFilter
     {
         [SerializeField] private Texture texture;
@@ -14,10 +15,20 @@ namespace GameFramework.UI.UITools
         [Range(0, 360)] public float rotation = 3;
         [Range(0, 1)] public float[] VerticesDistances = new float[3];
         public float size = 0;
-        public override Texture mainTexture => texture == null ? s_WhiteTexture : texture;
+        public override Texture mainTexture 
+        {
+            get
+            {
+                return   s_WhiteTexture;
+            }
+        }
         public Texture Texture
         {
-            get => texture;
+            get
+            {
+                
+                return texture;
+            }
             set
             {
                 if(texture != null && texture == value)return;
@@ -51,7 +62,7 @@ namespace GameFramework.UI.UITools
             thickness = Mathf.Clamp(thickness, 0, size / 2);
         }
 
-        protected UIVertex[] SetVertexBufferObject(Vector2[] vertices, Vector2[] uvs)
+        private UIVertex[] SetVertexBufferObject(Vector2[] vertices, Vector2[] uvs)
         {
             UIVertex[] VBO = new UIVertex[4];
             for (int i = 0; i < vertices.Length; i++)
@@ -127,7 +138,7 @@ namespace GameFramework.UI.UITools
                 RectTransformUtility.ScreenPointToLocalPointInRectangle(rectTransform, sp, eventCamera, out local);
                 float degrees = 360f / sides;
                 int vertices = sides + 1;
-                for (int i = 0; i < vertices-1; i++)
+                for (int i = 0; i < vertices; i++)
                 {
                     float outer = 0.5f * size * VerticesDistances[i];
                     float inner = 0.5f * size * VerticesDistances[i] - thickness;
@@ -148,72 +159,45 @@ namespace GameFramework.UI.UITools
                     }
                     prevX = pos1;
                     prevY = pos2;
+//                    if (fill)
+                    {
+                        if (prevX != Vector2.zero)
+                        {
+                            vectors.Add(prevX);
+                        }
+                    }
+//                    else
+                    {
+//                        if (prevY != Vector2.zero)
+//                        {
+//                            vectors.Add(prevY);
+//                        }
+                    }
+                }
 
-                    Vector2 pos = new Vector2(rectTransform.position.x,rectTransform.position.y);
-                    DrawLine(pos+pos0, pos+pos1, pos+pos2, pos+pos3, local);
-//                    if (!vectors.Contains(pos0))
-//                    {
-//                        vectors.Add(pos0);
-//                    }
-//                    if (!vectors.Contains(pos1))
-//                    {
-//                        vectors.Add(pos1);
-//                    }
-//                    if (!vectors.Contains(pos2))
-//                    {
-//                        vectors.Add(pos2);
-//                    }
-//                    if (!vectors.Contains(pos3))
-//                    {
-//                        vectors.Add(pos3);
-//                    }
-                    vectors.Add(pos0);
-                    vectors.Add(pos1);
-                    vectors.Add(pos2);
-                    vectors.Add(pos3);
-                } 
                 if (IsInPolygon(local, vectors))
                 {
                     return true;
                 }
-                
+//                if (fill)
+//                {
+//                    if (IsInPolygon(local, vectors))
+//                    {
+//                        return true;
+//                    }
+//                }
+//                else
+//                {
+//                    if (!IsInPolygon(local, vectors))
+//                    {
+//                        return true;
+//                    }
+//                }
             }
             return false;
         }
         
-        private bool IsInPolygon(Vector2 vertex0, Vector2 vertex1, Vector2 vertex2, Vector2 vertex3, Vector2 point)
-        {  
-            Bounds bounds = new Bounds(new Vector3(0.5f,0.5f,0f),Vector3.zero);
-            bounds.Encapsulate(vertex0);
-            bounds.Encapsulate(vertex1);
-            bounds.Encapsulate(vertex2);
-            bounds.Encapsulate(vertex3);
-            if (bounds.Contains(point))
-            {
-                return true;
-            }
-            return false;
-        }
-        
-        private bool DrawLine(Vector2 vertex0, Vector2 vertex1, Vector2 vertex2, Vector2 vertex3, Vector2 point)
-        {  
-            UnityEngine.Debug.DrawLine(vertex0,vertex1,Color.red,3);
-            UnityEngine.Debug.DrawLine(vertex1,vertex2,Color.red,3);
-            UnityEngine.Debug.DrawLine(vertex2,vertex3,Color.red,3);
-            UnityEngine.Debug.DrawLine(vertex3,vertex0,Color.red,3);
-//            Bounds bounds = new Bounds(new Vector3(0.5f,0.5f,0f),Vector3.zero);
-//            bounds.Encapsulate(vertex0);
-//            bounds.Encapsulate(vertex1);
-//            bounds.Encapsulate(vertex2);
-//            bounds.Encapsulate(vertex3);
-//            if (bounds.Contains(point))
-//            {
-//                return true;
-//            }
-            return false;
-        }
-        
-        public bool IsInPolygon(Vector2 checkPoint, List<Vector2> polygonPoints)
+        private bool IsInPolygon(Vector2 checkPoint, List<Vector2> polygonPoints)
         {
             bool inside = false;
             int pointCount = polygonPoints.Count;
@@ -240,47 +224,6 @@ namespace GameFramework.UI.UITools
                 }
             }
             return inside;
-        }
-        
-        public bool IsInPolygon2(Vector2 checkPoint, List<Vector2> polygonPoints)
-        {
-            int counter = 0;
-            int i;
-            double xinters;
-            Vector2 p1, p2;
-            
-            int pointCount = polygonPoints.Count;
-            p1 = polygonPoints[0];
-            for (i = 1; i <= pointCount; i++)
-            {
-                p2 = polygonPoints[i % pointCount];
-                if (checkPoint.y > Math.Min(p1.y, p2.y)//校验点的Y大于线段端点的最小Y 
-                    && checkPoint.y <= Math.Max(p1.y, p2.y))//校验点的Y小于线段端点的最大Y 
-                {
-                    if (checkPoint.x <= Math.Max(p1.x, p2.x))//校验点的X小于等线段端点的最大X(使用校验点的左射线判断). 
-                    {
-                        if (p1.y != p2.y)//线段不平行于X轴 
-                        {
-                            xinters = (checkPoint.y - p1.y) * (p2.x - p1.x) / (p2.y - p1.y) + p1.x;
-                            if (p1.x == p2.x || checkPoint.x <= xinters)
-                            {
-                                counter++;
-                            }
-                        }
-                    }
- 
-                }
-                p1 = p2;
-            }
- 
-            if (counter % 2 == 0)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
         }
     }
 }
